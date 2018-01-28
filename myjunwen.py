2e8f0d87276c3a9a42b8b1ccbf51256fc82bea6a
俊文 <344176791@qq.com>
2018-01-29 00:23:49 +0800

    tij

--- myjunwen.py
+++ myjunwen.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="xxx")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run(debug=False)
